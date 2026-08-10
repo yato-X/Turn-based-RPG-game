248b47fda1e2ffa64a934999aa23ef8de78f4bca
Nirjal Deshar <nirjaldeshar2@gmail.com>
2026-08-10 23:58:37 +0545

    enemy class and stats display

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,52 @@
 #include <iostream>
 using namespace std;
+
+class Enemy{
+    private:
+        string name;
+        int health;
+        int atkpower;
+    public:
+        Enemy(string name, int hp, int atk){
+        name = name;
+        health = hp;
+        atkpower = atk;
+        }
+    int getHealth(){
+        return health;
+    }
+    int getatkPower(){
+        return atkpower;
+    }
+    int getname(){
+        return name;
+    }
+    bool isalive(){
+        return health > 0;
+    }
+    void damage(int dmg){
+        health = health - dmg;
+        if(health<0){
+            health = 0;
+        }
+    }
+    void stats(){
+        cout<<name<<"(hp:)"<<health<<endl;
+        cout<<"Attack Power:"<<atkpower<<endl;
+    }
+};
+
+class Character{
+    private:
+        string name;
+        int health;
+        int atkpower;
+
+    public:
+        Character(string n, int hp, int atkpower);
+        
+
+};
 void showMenu(){
     cout<<"Menu"<<endl;
     cout<<"\n"<<endl;
@@ -13,7 +60,6 @@ int main(){
     while(running){
         showMenu();
     
-
 
     int choice;
     cin>> choice;
